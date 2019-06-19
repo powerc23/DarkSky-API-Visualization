9a4c14fce0088891b3ef5777fea61da4ceda4444
powerc23 <46970517+powerc23@users.noreply.github.com>
2019-06-19 22:28:44 +0100

    Add files via upload
    Creation of dictionary with longitude and latitude coordinates and initialization of API key

---
+++ api_data.py
@@ -0,0 +1,19 @@
+#Setting variable for DarkSky API key
+#api_key = *Unique API Key*
+
+
+#Create dictionary for storing longitude and latitude coordinate of cities
+coords = {'Dublin':(api_key,53.35,-6.26),'London': (api_key,51.51,-0.13),
+          'Madrid':(api_key,40.42,-3.7),'Rome':(api_key,41.9, 12.496),'Paris':(api_key,48.86,2.35),
+          'New York':(api_key,40.71,-74.01), 'San Francisco':(api_key, 37.77,-122.42),
+          'Moscow':(api_key,55.76,37.62), 'Beijing':(api_key,39.9,116.41), 'Rio de Janeiro':(api_key,-22.91,-43.17),
+          'Sydney':(api_key,-33.87,151.21), 'Vancouver':(api_key,49.15,-123.06),'Cape Town':(api_key,-33.56,18.25),
+          'Mumbai':(api_key,21.10,75.50),'Riyadh':(api_key,24.38,46.43), 'Marrakech':(api_key,31.38,8.01),'Prague':(api_key,50.05,14.25),
+          'Berlin':(api_key,52.31,13.23), 'Athens':(api_key,37.58,23.43), 'Stockholm':(api_key,59.51, 17.39), 'Auckland':(api_key,-36.50,174.44),
+          'Manila':(api_key,14.35,120.58), 'Bangkok':(api_key,13.45,100.28),'Baghdad':(api_key,33.20,44.26),
+          'Addis Ababa':(api_key,9.02,38.44),'Timbuktu':(api_key,16.47,-3.01), 'Bogota':(api_key,4.36,-74.05),
+          'Mexico City':(api_key,19.26,-99.08), 'Dallas':(api_key,32.47,-96.48), 'Buenos Aires':(api_key,-34.36,-58.23),
+          'Cairo':(api_key,30.03,31.14), 'Warsaw':(api_key,52.14,21.01), 'Kiev':(api_key,50.27,30.31)   }
+
+
+
